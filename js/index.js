ea411857d22a2dad406539c894f2e3cd7dd6c756
hw95 <43203062+hw95@users.noreply.github.com>
2021-10-13 14:02:34 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-﻿var arr = "2020-01-28 12:00:00".split(/[- :]/);//改这里
+﻿var arr = "2020-01-27 00:00:00".split(/[- :]/);//改这里
 var box = document.getElementsByClassName('textCon')[0];
 function init() {
     var width = box.offsetWidth;
